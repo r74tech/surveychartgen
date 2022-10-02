b9f10400f728d30b33325a938d295f316dd83cf8
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-10-02 21:30:06 +0900

    fix

--- src/components/Chartcodegen.vue
+++ src/components/Chartcodegen.vue
@@ -275,6 +275,10 @@ export default {
             let mytype = myChart["mytype"];
             let textChartData = JSON.stringify(ChartData);
             let textOption = JSON.stringify(myoption);
+            let cout__m = document.getElementById("cout__m") as HTMLInputElement;
+            let cout__s = document.getElementById("cout__s") as HTMLInputElement;
+            let title = ChartData["datasets"][0]["label"];
+            ChartData["datasets"][0]["label"] = "回答数";
             let codecontent = `new Chart($("#cha"), {
                 type: "${mytype}",
                 data: ${textChartData},
@@ -284,10 +288,6 @@ export default {
             let usetemplate = document.getElementById(
                 "usetemplate"
             ) as HTMLInputElement;
-            let cout__m = document.getElementById("cout__m") as HTMLInputElement;
-            let cout__s = document.getElementById("cout__s") as HTMLInputElement;
-            let title = ChartData["datasets"][0]["label"];
-            ChartData["datasets"][0]["label"] = "回答数";
             let templatetype, t_type;
             switch (usetemplate.value) {
                 case "acc":
